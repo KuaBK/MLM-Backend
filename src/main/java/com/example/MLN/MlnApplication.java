d4747ef9a2894d13739786d473ecfa41a6419b33
TGG-Henry <tuanphonglqd@gmail.com>
2026-04-22 14:49:46 +0700

    init

---
+++ src/main/java/com/example/MLN/MlnApplication.java
@@ -0,0 +1,13 @@
+package com.example.MLN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MLN/MlnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MLN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
